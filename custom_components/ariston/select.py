57f031f58728c3bc15eae3ae0c638d67086f09c7
sasamix <asm@pioner.kz>
2026-09-24 16:18:50 +0500

    Tidy scenario control names and grouping

--- custom_components/ariston/select.py
+++ custom_components/ariston/select.py
@@ -76,7 +76,7 @@ class AristonHeatingScenarioSelect(AristonEntity, SelectEntity):
     def __init__(self, coordinator, manager, zone: int) -> None:
         description = AristonHeatingSelectEntityDescription(
             key=f"HeatingScenarioZone{zone}",
-            name=f"Ariston CH heating scenario zone {zone}",
+            name=f"Ariston CH scenario zone {zone}",
             icon="mdi:radiator",
             entity_category=EntityCategory.CONFIG,
         )
